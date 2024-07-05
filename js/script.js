588fa41746694400c2faa4d889dda3c3c616b10a
Javier Jacobo <173719689+JavierE-J@users.noreply.github.com>
2024-07-05 17:35:32 +0000

    Added Files and a bit of JS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let canvasWidth=800
+let canvasHeight=500
+
+
+
+
+
+function setup() {
+    createCanvas(800, 500);
+    background(240, 233, 254);
+  }
+  
+  function draw() {
+    background(240, 233, 254);
+  }
